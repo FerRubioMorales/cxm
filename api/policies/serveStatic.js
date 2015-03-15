f78d42ca40c9cb315daea13bf03d3aacf0022939
cronosmen <cronosmen@gmail.com>
2015-03-15 23:57:11 +0100

    Deleted console logs
    deleted console logs for debug dynamic assets load

--- api/policies/serveStatic.js
+++ api/policies/serveStatic.js
@@ -3,9 +3,9 @@
  */
 var fs = require('fs');
 module.exports = function(req, res, next) {
-    console.log(fs.existsSync(sails.config.asset.js_dir_path + '/' + req.options.controller + '/js/' + req.options.action + '.js'));
     res.locals.scripts = [];
     res.locals.styles = [];
+
     if(fs.existsSync(sails.config.asset.js_dir_path + '/' + req.options.controller + '/js/common.js'))
         res.locals.scripts.push(sails.config.asset.js_public_path+ '/' + req.options.controller + '/js/common.js');
     if(fs.existsSync(sails.config.asset.css_dir_path + '/' + req.options.controller + '/css/common.css'))
